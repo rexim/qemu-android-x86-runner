f4e29ce0151a1ad955251b5facd31a7fea8aaca9
rexim <reximkut@gmail.com>
2018-02-28 18:31:35 +0700

    Extra parameters for `qemu-run.sh` script

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 - Download `android-x86_64-7.1-r1.iso` from http://www.android-x86.org/download
 - `$ ./qemu-install.sh ./android-x86_64-7.1-r1.iso ./android.img`
 - `$ ./qemu-run.sh ./android.img`
+- `$ ./qemu-run.sh ./android.img -vnc localhost:0` -- run with VNC listener on for local incoming connections on 5900 port
 
 [QEMU]: https://www.qemu.org/
--- qemu-run.sh
+++ qemu-run.sh
@@ -2,10 +2,15 @@
 
 set -xe
 
+HDA_IMAGE="$1"
+
+shift
+
 qemu-system-x86_64 -enable-kvm -vga std \
                    -m 2048 -smp 2 -cpu host \
                    -soundhw ac97 \
                    -net nic,model=e1000 -net user \
-                   -hda "$1" \
-                   -monitor stdio
+                   -hda "$HDA_IMAGE" \
+                   -monitor stdio \
+                   "$@"
 
