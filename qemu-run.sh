90af1286e01d49d3481fbd5e4ef20b39db3251fd
rexim <reximkut@gmail.com>
2018-03-05 19:31:23 +0700

    Forward 9999 udp port

--- qemu-run.sh
+++ qemu-run.sh
@@ -9,7 +9,8 @@ shift
 qemu-system-x86_64 -enable-kvm -vga std \
                    -m 2048 -smp 2 -cpu host \
                    -soundhw ac97 \
-                   -net nic,model=e1000 -net user \
+                   -device e1000,netdev=net0 \
+                   -netdev user,id=net0,hostfwd=udp::9999-:9999 \
                    -hda "$HDA_IMAGE" \
                    -monitor stdio \
                    "$@"
