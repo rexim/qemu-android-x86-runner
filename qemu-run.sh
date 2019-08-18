8710a3dcb96a1db9fb6843d1c7843eb5ac75fc6e
rexim <reximkut@gmail.com>
2019-08-18 21:18:02 +0700

    (#1) Remove 9999 UDP port forward
    It's irrelevant to this project

--- qemu-run.sh
+++ qemu-run.sh
@@ -10,7 +10,7 @@ qemu-system-x86_64 -enable-kvm -vga std \
                    -m 2048 -smp 2 -cpu host \
                    -soundhw ac97 \
                    -device e1000,netdev=net0 \
-                   -netdev user,id=net0,hostfwd=udp::9999-:9999 \
+                   -netdev user,id=net0 \
                    -hda "$HDA_IMAGE" \
                    -monitor stdio \
                    "$@"
